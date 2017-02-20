c83b2c5710ff91ec8ee239d642657f1d4bb064fb
theJPster <github@thejpster.org.uk>
2017-02-20 11:44:17 +0000

    Updated example.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ extern crate sensehat;
 
 fn main() {
     let mut hat = sensehat::SenseHat::new().expect("Couldn't find Sense Hat");
-    let temp = hat.get_temperature_from_humidity();
-    println!("Hello, world!");
+    let temp = hat.get_temperature_from_humidity().expect("Couldn't read temp");
+    println!("Hello, world! It's {}", temp);
 }
